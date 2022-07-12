e103d012a2be71edc7f603a1a3e819b51152646e
nik.volkov <nikita.volkov@jetbrains.com>
2022-07-13 01:11:22 +0300

    cleared index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 import kvFactory, { SessionStorageEngine } from './kv-storage'
-import { RequestBuilder } from './requestBuilder';
-
 
 const ls = kvFactory('user', {engine: new SessionStorageEngine()})
 
-const request = new RequestBuilder()
-
-request.setUrl('www.example.com')
-request.setMethod('POST')
-const makeRequest = request.build()
-
-makeRequest.fetch()
